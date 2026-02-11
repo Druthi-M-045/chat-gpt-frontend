58d5d87f434394e35017225deac2fbe865b2fff7
Druthi-M-045 <druthimsgowda@gmail.com>
2026-02-11 12:48:32 +0530

    Login-Signup

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,19 +3,25 @@ import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
 function App() {
   return (
     <Router>
-      <Header />
+      <Header/>
       
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
-      <Footer />
+      <Footer/>
     </Router>
   )
 }
